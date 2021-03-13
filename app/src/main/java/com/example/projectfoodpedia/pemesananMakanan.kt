24eb2412e2645bf6bca64dc82381e697aee2ec1a
marbelbel0 <marbelbel0@gmail.com>
2021-03-13 19:19:55 +0700

    first commit

---
+++ app/src/main/java/com/example/projectfoodpedia/pemesananMakanan.kt
@@ -0,0 +1,11 @@
+package com.example.projectfoodpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pemesananMakanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pemesanan_makanan)
+    }
+}
---
+++ app/src/main/java/com/example/projectfoodpedia/penjelasanMakanan.kt
@@ -0,0 +1,11 @@
+package com.example.projectfoodpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class penjelasanMakanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_penjelasan_makanan)
+    }
+}
