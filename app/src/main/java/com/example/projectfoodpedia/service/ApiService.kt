b3ef5936c4a4b4d565d0693d116ec74c50d5d0ac
marbelbel0 <marbelbel0@gmail.com>
2021-03-21 11:26:41 +0700

    retrieve data dari api

--- app/src/main/java/com/example/projectfoodpedia/service/ApiService.kt
+++ app/src/main/java/com/example/projectfoodpedia/service/ApiService.kt
@@ -1,8 +1,10 @@
 package com.example.projectfoodpedia.service
 
 import com.example.projectfoodpedia.datamakanan.ListCategoryResponse
+import com.example.projectfoodpedia.datamakanan.ListMealDetails
 import com.example.projectfoodpedia.datamakanan.ListMealResponse
 import com.example.projectfoodpedia.utils.Constant
+import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,15 +18,15 @@ class ApiService {
     .build()
     .create(MealsApi::class.java)
 
-    fun getListCategories(): ListCategoryResponse{
+    fun getListCategories(): Observable<ListCategoryResponse> {
         return retrofit.getListCategories()
     }
 
-    fun getListMeal(c: String): ListMealResponse {
+    fun getListMeal(c: String): Observable<ListMealResponse> {
         return retrofit.getListMeal(c)
     }
 
-    fun getListMeal(c: String): ListMealResponse {
-        return retrofit.getListMeal(c)
+    fun getDetails(i: String): Observable<ListMealDetails> {
+        return retrofit.getDetails(i)
     }
 }
--- app/src/main/java/com/example/projectfoodpedia/service/MealsApi.kt
+++ app/src/main/java/com/example/projectfoodpedia/service/MealsApi.kt
@@ -4,20 +4,22 @@ import com.example.projectfoodpedia.datamakanan.CategoryResponse
 import com.example.projectfoodpedia.datamakanan.ListCategoryResponse
 import com.example.projectfoodpedia.datamakanan.ListMealDetails
 import com.example.projectfoodpedia.datamakanan.ListMealResponse
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MealsApi {
     @GET ("categories.php")
-    fun getListCategories(): ListCategoryResponse
+    fun getListCategories(): Observable<ListCategoryResponse> //observable bakal nungguin data terus || ada lagi single yang cuman sekali nerima
+
 
     @GET ("filter.php")
-    fun getListMeal(@Query("c") c : String): ListMealResponse
+    fun getListMeal(@Query("c") c : String): Observable<ListMealResponse>
 
 //    @GET ("filter.php?c={category}")
 //    fun getListMeal(@Path("category") category : String): ListMealResponse
 
     @GET ("lookup.php")
-    fun getCategories(@Query("i") i : String): ListMealDetails
+    fun getDetails(@Query("i") i : String): Observable<ListMealDetails>
 }
