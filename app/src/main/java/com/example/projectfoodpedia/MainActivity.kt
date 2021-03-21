710198e87ec17ee02d06284bf4bd28d9ddcc7cd2
marbelbel0 <marbelbel0@gmail.com>
2021-03-21 11:50:24 +0700

    new api

--- app/src/main/java/com/example/projectfoodpedia/MainActivity.kt
+++ app/src/main/java/com/example/projectfoodpedia/MainActivity.kt
@@ -9,12 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setContentView(R.layout.activity_main)
-        //val button: Button = findViewById(R.id.button_id)
-        //button.setOnClickListener(object : OnClickListener() {
-            //fun onClick(v: View?) {
-                // Code here executes on main thread after user presses button
-            //}
-        //})
-    }
+//        setContentView(R.layout.activity_main)
+//        val button: Button = findViewById(R.id.button_id)
+//
+  }
 }
